c287ed00caae3a9cadb0d0735b53a90f4131a43c
Aidan <starke.aidan@gmail.com>
2025-10-04 22:45:31 +1300

    non symlink theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,1 +1,24 @@
-/home/aidan/.config/omarchy/current/theme/neovim.lua
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+      on_highlights = function(hl, c)
+        hl.Keyword = { fg = c.blue5 }
+        hl.Comment = { fg = "#636da6" }
+
+        hl.LineNr = { fg = c.blue, bg = "none" }
+        hl.LineNrAbove = { fg = c.blue0, bg = "none" }
+        hl.LineNrBelow = { fg = c.blue0, bg = "none" }
+        hl.CursorLineNr = { fg = c.orange, bold = true, bg = "none" }
+        hl.SignColumn = { bg = "none" }
+      end,
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+}
