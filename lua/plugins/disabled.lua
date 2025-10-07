f05a0b728c707a2cc115b058d3c0ff86a4b8e4e1
Aidan Starke <starke.aidan@gmail.com>
2025-10-08 11:46:49 +1300

    mini surround

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -4,7 +4,6 @@ return {
   { "persistence.nvim", enabled = false },
   { "folke/trouble.nvim", enabled = false },
   { "CopilotChat.nvim", enabled = false },
-  { "mini/surround.nvim", enabled = false },
   { "neogen", enabled = false },
   { "aerial.nvim", enabled = false },
   { "dial.nvim", enabled = false },
--- lua/plugins/mine.lua
+++ lua/plugins/mine.lua
@@ -8,15 +8,6 @@ return {
   { "MeanderingProgrammer/render-markdown.nvim", ft = "markdown" }, -- Render markdown
 
   {
-    "kylechui/nvim-surround",
-    version = "*",
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup()
-    end,
-  }, -- Edit surrounding brackets/quotes/tags
-
-  {
     "gcmt/wildfire.vim",
     event = "VeryLazy",
   }, -- Smart selection
